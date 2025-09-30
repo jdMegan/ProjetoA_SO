84f2470f390bb419dbde6a4d4f26c806c4a88bd8
Jade Macagnan de Simão <145635173+jdMegan@users.noreply.github.com>
2025-09-30 16:58:36 -0300

    Create Parser class with constructor and lerConfigs method
    Initial implementation of the Parser class with a constructor and a method for reading configurations.
    Nada nele esta de fato implementado, é um placeholder por enquanto.

---
+++ Parser.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/**
+ * Write a description of class Parser here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Parser
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * Constructor for objects of class Parser
+     */
+    public Parser()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public ArrayList<Integer> lerConfigs()
+    {
+        ArrayList<Integer> configs = null;
+        //Ok
+        //Aqui a gente le as configuraçoes e salva elas em um array
+        return configs;
+    }
+}   
