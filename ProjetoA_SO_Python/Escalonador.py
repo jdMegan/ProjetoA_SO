9e121b074b3405d60e985f6d9fc26f73ab6fc06e
Jade Macagnan de Simão <145635173+jdMegan@users.noreply.github.com>
2025-10-03 23:47:23 -0300

    Update Escalonador.py

--- ProjetoA_SO_Python/Escalonador.py
+++ ProjetoA_SO_Python/Escalonador.py
@@ -1,3 +1,21 @@
 class Escalonador():
     def __init__(self):
-        pass
+        self._quantum = None
+        self._alg = None
+
+#Os tipos de algoritimo poderiam ser um ENUM, implementar isso no futuro
+
+    # Getters e setters
+    @property 
+    def quantum(self):
+        return self._quantum
+    @quantum.setter
+    def quantum(self, quantum):
+        self._quantum = quantum
+
+    @property 
+    def alg(self):
+        return self._alg
+    @alg.setter
+    def alg(self, alg):
+        self._alg = alg
