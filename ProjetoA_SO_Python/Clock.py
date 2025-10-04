206b34c55b9e249c066f83cca099de8699085c1f
Jade Macagnan de Simão <145635173+jdMegan@users.noreply.github.com>
2025-10-03 23:44:43 -0300

    Update Clock.py
    Definição do Clock e implementação do incremento de tempo

--- ProjetoA_SO_Python/Clock.py
+++ ProjetoA_SO_Python/Clock.py
@@ -1,3 +1,22 @@
 class Clock():
     def __init__(self):
-        pass
+        self._tick = 1
+        self._tickAtual = 0
+
+    def proximoTick(self):
+        """Avança o tempo e retorna o novo tickAtual"""
+        self._tickAtual += self._tick
+        return self._tickAtual
+    
+    @property
+    def tickAtual(self):
+        return self._tickAtual
+    
+    @property
+    def tick(self):
+        return self._tick
+    @tick.setter
+    def tick(self, valor):
+        if valor <= 0:
+            raise ValueError("Tick deve ser positivo")
+        self._tick = valor
