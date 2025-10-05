cb1d371b36f0f33d8edd3daad2792a4dda362ee7
Jade Macagnan de Simão <145635173+jdMegan@users.noreply.github.com>
2025-10-05 00:43:23 -0300

    Update Escalonador.py

--- ProjetoA_SO_Python/Escalonador.py
+++ ProjetoA_SO_Python/Escalonador.py
@@ -1,9 +1,26 @@
-class Escalonador():
+from Enums import AlgoritmoEscalonamento
+
+class Escalonador:
     def __init__(self):
         self._quantum = None
         self._alg = None
 
 #Os tipos de algoritimo poderiam ser um ENUM, implementar isso no futuro
+
+    #Recebe a lista de prontas, escolhe uma, retorna ela pro so por na lista de executando
+    def escolherTarefa(self, listaProntas):
+        if self._alg == AlgoritmoEscalonamento.FCFS:
+            return listaProntas.getNext()
+        elif self._alg == AlgoritmoEscalonamento.PRIORIDADE_P:
+            # Implementação futura
+            pass
+        elif self._alg == AlgoritmoEscalonamento.SRTF:
+            # Implementação futura  
+            pass
+        return None
+    
+    
+
 
     # Getters e setters
     @property 
@@ -19,3 +36,4 @@ class Escalonador():
     @alg.setter
     def alg(self, alg):
         self._alg = alg
+
