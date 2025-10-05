570e58807d4e36926666c3cf1bf26bdcb70302ca
Jade Macagnan de Simão <145635173+jdMegan@users.noreply.github.com>
2025-10-05 00:42:16 -0300

    Update SistemaOperacional.py

--- ProjetoA_SO_Python/SistemaOperacional.py
+++ ProjetoA_SO_Python/SistemaOperacional.py
@@ -9,46 +9,49 @@ import FilaTarefas
 import Parser
 import Escalonador
 import TCB
-class SistemaOperacional():
+class SistemaOperacional:
     def __init__(self,nome_config):
-
         self._relogio = Clock.Clock()
+        #As tarefas são divididas em objetos FilaTarefas dependendo do seu estado
         self._tarefasCarregadas = FilaTarefas.FilaTarefas()
         self._tarefasProntas = FilaTarefas.FilaTarefas()
         self._tarefasSuspensas = FilaTarefas.FilaTarefas()
         self._tarefaExecutando = FilaTarefas.FilaTarefas()
         self._escalonador = Escalonador.Escalonador()
         self._parser = Parser.Parser()
+        #Configuraçoes do escalonador e das tarefas, respectivamente
         self._configuracoes = None
         self._tarefas = []
-
+        #Recebe o nome do arquivo com as configurações para começar
         self.comecar(nome_config)
 
     def comecar(self,nome_config):
         print("Iniciando Sistema Operacional...")
-
+        print("=== Tick 0 ===")
+        #Configuraçoes
         self.parseConfigs(nome_config)
         self.configsEscalonador()
         self.criarTasks()
-        self.proximoTick()
-
-        #loopConstante();  
+        #Começa loop
+        self.loopConstante();  
 
     def parseConfigs(self, nome_config):
         print("Lendo configurações...")
-        # configuraçoes: algoritmo_escalonamento; quantum. Pro escalonador
-        # tarefas: id; cor; ingresso; duracao; prioridade; lista_eventos. Pras TCB
+        # Configuraçoes: algoritmo_escalonamento; quantum. Pro escalonador
+        # Tarefas: id; cor; ingresso; duracao; prioridade; lista_eventos. Pras TCB
         self._configuracoes, self._tarefas = self._parser.lerConfigs(nome_config)
-    
 
     def criarTasks(self):
         print("Criando tasks...")
+        #_tarefas é um list
+        #Cada elemento é um string com dados de uma unica task separados por ;
+        #Aqui separa esses dados e usa para criar task
         for tarefa in self._tarefas:
             dados = tarefa.split(';')
             print(dados)
             novoTCB = TCB.TCB(
                 id=dados[0],
-                cor=dados[1],
+                cor=int(dados[1]),  # O TCB converte pra Enum
                 ingresso=int(dados[2]),
                 duracao=int(dados[3]),
                 eventos=dados[4:]
@@ -56,27 +59,57 @@ class SistemaOperacional():
             print(novoTCB)
             self._tarefasCarregadas.addTask(novoTCB)
         
-
     def configsEscalonador(self):
         print("Configurando escalonador...")
-        self._escalonador.alg  = self._configuracoes[0]
-        self._escalonador.quantum  = int(self._configuracoes[1])
-
+        self._escalonador.alg = self._configuracoes[0] #ENUM
+        self._escalonador.quantum = self._configuracoes[1] #int
 
     def atualizaHistorico():
-        # atualiza o historico a cada tick
+        # Atualiza o historico a cada tick
         pass
 
     def proximoTick(self):
-        """Avança o tempo no sistema"""
-        novo_tick = self._relogio.proximoTick()
-        print(f"=== Tick {novo_tick} ===")
-        
-        # Aqui você vai chamar:
-        #self._atualizarEstadoTarefas()  # Tarefas que chegam neste tick
-        #self._escalonador.Escalonar()   # Escalonador decide próxima tarefa
-        #self._executar_tarefa()         # Executa a tarefa atual
-        #self._historico.atualizarHistorico()       # Registra estado atual
-        
-        #return novo_tick
-        
+        #Avança o tempo
+        novoTick = self._relogio.proximoTick()
+        print(f"=== Tick {novoTick} ===")
+
+    def atualizaCarregadas(self, tickAtual):
+        #Cria uma lista temporaria para poder remover durante a iteração
+        mover = [t for t in self._tarefasCarregadas.getAll() if t.ingresso == tickAtual]
+        #As que ingresso no tickAtual vao para a fila de prontas
+        for t in mover:
+            t.estado = "pronta"
+            self._tarefasProntas.addTask(t)
+            self._tarefasCarregadas.removeTask(t)
+   
+    def loopConstante(self):
+        # No momento o loop para qndo todas as tarefas estiverem na fila de prontas
+        # No futuro vai parar quando nao tiver mais nenhuma pronta e executando
+        # while not (self._tarefasCarregadas.isEmpty() 
+        #   and self._tarefasProntas.isEmpty() 
+        #   and self._tarefaExecutando.isEmpty()):
+        while not self._tarefasCarregadas.isEmpty() :
+            #Ve se alguma tarefa entra agora
+            self.atualizaCarregadas(self._relogio.tickAtual)
+            
+            # Escalonador decide próxima tarefa
+            taferaExecutar = self._escalonador.escolherTarefa(self._tarefasProntas)   
+            self._tarefaExecutando.addTask(taferaExecutar)
+
+            # Executa a tarefa atual
+            #self.executarTarefa()
+
+            # Atualiza hitorico
+            ####self._historico.atualizarHistorico()
+            
+            #Passa o tempo
+            self.proximoTick()
+
+    #Isso ta errado
+    #Não faz mto sentido tarefaExecutando estar em uma lista ja q é só uma tarefa executando por vez
+    #Mas n sei. . . tranformar ela em um atributo entao?
+    #def executarTarefa(self):
+        #if (self._tarefaExecutando[0].estaConcluida):
+        #    self._tarefaExecutando[0].concluirTarefa
+        #    #Quando é concluida é removida da memoria
+        #    self._tarefaExecutando.getNext()
